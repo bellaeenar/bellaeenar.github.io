288821f9ee939788c45746e7069cce2237425de6
bella@Eenar <bellaeenar@gmail.com>
2022-04-19 03:44:38 +0000

    add header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -8,8 +9,11 @@ export default function Home() {
         <meta name="description" content="info about bella" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header />
       <main>
+        {/* sidebar */}
+        {/* feed */}
+        {/* widgets */}
         <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1>
